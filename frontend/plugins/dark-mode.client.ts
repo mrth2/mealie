5c574172b43082b2a7e5679dc3f76c2f987b8f16
mrth2 <mr.th2.90@gmail.com>
2025-03-02 13:47:35 +0000

    refactor: comment out dark mode polling logic in dark-mode.client.ts

--- frontend/plugins/dark-mode.client.ts
+++ frontend/plugins/dark-mode.client.ts
@@ -2,18 +2,18 @@ import { useDark } from "@vueuse/core";
 
 export default defineNuxtPlugin(() => {
   const isDark = useDark();
-  const { $vuetify } = useNuxtApp();
+  // const { $vuetify } = useNuxtApp();
 
-  // Vuetify metadata is bugged and doesn't render dark mode fully when called immediately
-  // Adding a delay fixes this problem
-  // https://stackoverflow.com/questions/69399797/vuetify-darkmode-colors-wrong-after-page-reload
-  const toggleDarkThemePolling = () => {
-    if ($vuetify) {
-      $vuetify.theme.global.name.value = isDark.value ? 'dark' : 'light';
-    }
-    else setTimeout(toggleDarkThemePolling, 200);
-  }
-  toggleDarkThemePolling();
+  // // Vuetify metadata is bugged and doesn't render dark mode fully when called immediately
+  // // Adding a delay fixes this problem
+  // // https://stackoverflow.com/questions/69399797/vuetify-darkmode-colors-wrong-after-page-reload
+  // const toggleDarkThemePolling = () => {
+  //   if ($vuetify) {
+  //     $vuetify.theme.global.name.value = isDark.value ? 'dark' : 'light';
+  //   }
+  //   else setTimeout(toggleDarkThemePolling, 200);
+  // }
+  // toggleDarkThemePolling();
   return {
     provide: {
       isDark,
