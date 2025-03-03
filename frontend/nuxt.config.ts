ea8b86215a562c6ef9f4d46e70a216e07e3c0b87
mrth2 <mr.th2.90@gmail.com>
2025-03-03 08:33:55 +0000

    nuxt 3 migration: feat: disable server-side rendering for improved performance

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,7 +4,7 @@ import commonjs from 'vite-plugin-commonjs';
 export default defineNuxtConfig({
   // Global page headers: https://go.nuxtjs.dev/config-head
   // target: "static",
-  ssr: true,
+  ssr: false,
   future: {
     compatibilityVersion: 3,
   },
