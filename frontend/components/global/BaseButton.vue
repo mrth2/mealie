028ff7a69d3caae66cd8b0d5a879235fecd2e026
mrth2 <mr.th2.90@gmail.com>
2025-03-07 13:20:28 +0000

    vuetify 3 - refactor: update BaseButton component to enhance variant handling for consistent styling

--- frontend/components/global/BaseButton.vue
+++ frontend/components/global/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <v-btn :color="color || btnAttrs.color" :size="small ? 'small' : 'default'" :x-small="xSmall" :loading="loading"
-    :disabled="disabled" :outlined="btnStyle.outlined" :variant="btnStyle.text ? 'text' : 'elevated'" :to="to"
+    :disabled="disabled" :variant="btnStyle.outlined ? 'outlined' : btnStyle.text ? 'text' : 'elevated'" :to="to"
     v-bind="$attrs" v-on="$attrs" @click="download ? downloadFile() : undefined">
     <v-icon v-if="!iconRight" start>
       <slot name="icon">
