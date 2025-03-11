738891e170f41e40ba85b51c7d0a8e2bde5e6fec
mrth2 <mr.th2.90@gmail.com>
2025-03-11 16:01:02 +0000

    refactor: simplify locale handling in useLocales for improved clarity and performance

--- frontend/composables/use-locales/use-locales.ts
+++ frontend/composables/use-locales/use-locales.ts
@@ -5,36 +5,25 @@ import { LOCALES } from "./available-locales";
 export const useLocales = () => {
   const i18n = useI18n();
 
-  function getLocale(value: string) {
-    const currentLocale = LOCALES.filter((locale) => locale.value === value);
-    return currentLocale.length ? currentLocale[0] : null;
-  }
   const { isRtl } = useRtl();
-  const { current } = useLocale();
+  const { current: vuetifyLocale } = useLocale();
 
   const locale = computed<LocaleObject['code']>({
-    get() {
-      // dirty hack
-      const currentLocale = getLocale(current.value);
-      if (currentLocale) {
-        isRtl.value = currentLocale.dir === "rtl";
-      }
-
-      return i18n.locale.value;
-    },
+    get: () => i18n.locale.value,
     set(value) {
       i18n.setLocale(value);
-
-      // this does not persist after window reload :-(
-      current.value = value;
-      const currentLocale = getLocale(value);
-      if (currentLocale) {
-        isRtl.value = currentLocale.dir === "rtl";
-      }
-
-      // Reload the page to update the language - not all strings are reactive
-      window.location.reload();
-    },
+    }
+  })
+  // auto update vuetify locale
+  watch(locale, (lc) => {
+    vuetifyLocale.value = lc;
+  });
+  // auto update rtl
+  watch(vuetifyLocale, (vl) => {
+    const currentLocale = LOCALES.find(lc => lc.value === vl);
+    if (currentLocale) {
+      isRtl.value = currentLocale.dir === "rtl";
+    }
   });
 
   return {
