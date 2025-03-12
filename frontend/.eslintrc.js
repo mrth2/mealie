82091e1f7823ddfbb3eb8a56bd601d5b2d9de2bd
mrth2 <mr.th2.90@gmail.com>
2025-03-12 15:46:42 +0000

    fix: add 'vue/no-duplicate-attributes' rule to ESLint configuration for improved code quality

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     "@nuxtjs/eslint-config-typescript",
     "plugin:nuxt/recommended",
     "eslint:recommended",
+    "vue/no-duplicate-attributes",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     // "plugin:prettier/recommended",
