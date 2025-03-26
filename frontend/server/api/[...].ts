0ca6e52fdcf3d40e1be4c06747b9e5f9f07f0128
mrth2 <mr.th2.90@gmail.com>
2025-03-26 03:35:02 +0000

    fix: add redirect for OAuth login in API handler

--- frontend/server/api/[...].ts
+++ frontend/server/api/[...].ts
@@ -4,6 +4,9 @@ export default defineEventHandler(async (event) => {
   const apiUrl = useRuntimeConfig().apiUrl; // 'http://localhost:9000'
 
   const target = joinURL(apiUrl, event.path);
+  if (event.path === "/api/auth/oauth") {
+    return sendRedirect(event, target); // redirect to BE for Oauth login
+  }
 
   return proxyRequest(event, target);
 });
