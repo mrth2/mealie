0aa5c35491cb59091b8ab75feee1bcc5f73e5519
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2025-06-16 20:13:26 +0000

    formatting

--- frontend/components/Domain/Recipe/RecipePage/RecipePageParts/RecipePageInfoEditor.vue
+++ frontend/components/Domain/Recipe/RecipePage/RecipePageParts/RecipePageInfoEditor.vue
@@ -1,71 +1,80 @@
 <template>
   <div>
-    <v-text-field v-model="recipe.name"
-                  class="my-3"
-                  :label="$t('recipe.recipe-name')"
-                  :rules="[validators.required]"
-                  density="compact"
-                  variant="underlined"
+    <v-text-field
+      v-model="recipe.name"
+      class="my-3"
+      :label="$t('recipe.recipe-name')"
+      :rules="[validators.required]"
+      density="compact"
+      variant="underlined"
     />
     <v-container class="ma-0 pa-0">
       <v-row>
         <v-col cols="3">
-          <v-text-field :model-value="recipeServings"
-                        type="number"
-                        :min="0"
-                        hide-spin-buttons
-                        density="compact"
-                        :label="$t('recipe.servings')"
-                        variant="underlined"
-                        @update:model-value="validateInput($event, 'recipeServings')"
+          <v-text-field
+            :model-value="recipeServings"
+            type="number"
+            :min="0"
+            hide-spin-buttons
+            density="compact"
+            :label="$t('recipe.servings')"
+            variant="underlined"
+            @update:model-value="validateInput($event, 'recipeServings')"
           />
         </v-col>
         <v-col cols="3">
-          <v-text-field :model-value="recipeYieldQuantity"
-                        type="number"
-                        :min="0"
-                        hide-spin-buttons
-                        density="compact"
-                        :label="$t('recipe.yield')"
-                        variant="underlined"
-                        @update:model-value="validateInput($event, 'recipeYieldQuantity')"
+          <v-text-field
+            :model-value="recipeYieldQuantity"
+            type="number"
+            :min="0"
+            hide-spin-buttons
+            density="compact"
+            :label="$t('recipe.yield')"
+            variant="underlined"
+            @update:model-value="validateInput($event, 'recipeYieldQuantity')"
           />
         </v-col>
         <v-col cols="6">
-          <v-text-field v-model="recipe.recipeYield"
-                        density="compact"
-                        :label="$t('recipe.yield-text')"
-                        variant="underlined"
+          <v-text-field
+            v-model="recipe.recipeYield"
+            density="compact"
+            :label="$t('recipe.yield-text')"
+            variant="underlined"
           />
         </v-col>
       </v-row>
     </v-container>
 
-    <div class="d-flex flex-wrap"
-         style="gap: 1rem"
+    <div
+      class="d-flex flex-wrap"
+      style="gap: 1rem"
     >
-      <v-text-field v-model="recipe.totalTime"
-                    :label="$t('recipe.total-time')"
-                    density="compact"
-                    variant="underlined"
+      <v-text-field
+        v-model="recipe.totalTime"
+        :label="$t('recipe.total-time')"
+        density="compact"
+        variant="underlined"
       />
-      <v-text-field v-model="recipe.prepTime"
-                    :label="$t('recipe.prep-time')"
-                    density="compact"
-                    variant="underlined"
+      <v-text-field
+        v-model="recipe.prepTime"
+        :label="$t('recipe.prep-time')"
+        density="compact"
+        variant="underlined"
       />
-      <v-text-field v-model="recipe.performTime"
-                    :label="$t('recipe.perform-time')"
-                    density="compact"
-                    variant="underlined"
+      <v-text-field
+        v-model="recipe.performTime"
+        :label="$t('recipe.perform-time')"
+        density="compact"
+        variant="underlined"
       />
     </div>
-    <v-textarea v-model="recipe.description"
-                auto-grow
-                min-height="100"
-                :label="$t('recipe.description')"
-                density="compact"
-                variant="underlined"
+    <v-textarea
+      v-model="recipe.description"
+      auto-grow
+      min-height="100"
+      :label="$t('recipe.description')"
+      density="compact"
+      variant="underlined"
     />
   </div>
 </template>
